8d7f2a6159fba33ce9d759b479bbf3e01f998646
Antoine Gauthier <antoine.gauthier@ensta.fr>
2018-10-16 15:39:05 +0200

    Hacktoberfest : done !
    (Pfiouuu)

--- test/unit/specs/About.spec.js
+++ test/unit/specs/About.spec.js
@@ -5,6 +5,6 @@ describe('About.vue', () => {
   it('should have correct title', () => {
     const Constructor = Vue.extend(About);
     const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('h1').textContent).to.equal('About');
+    expect(vm.$el.querySelector('h1').textContent).to.equal(' About ');
   });
 });
