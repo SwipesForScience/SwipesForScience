823ef7425ba5f4fc628d55d4b7c545313f08965f
Antoine Gauthier <antoine.gauthier@ensta.fr>
2018-10-26 14:03:50 +0200

    Added new Widget with tests

--- test/unit/specs/config.spec.js
+++ test/unit/specs/config.spec.js
@@ -23,7 +23,7 @@ describe('config.js', () => {
   });
 
   it('widgetType should be a valid widget', () => {
-    const widgetKeys = ['ImageSplat', 'ImageSwipe', 'ImageSoundSwipe', 'PubMedNLP', 'TemplateWidget'];
+    const widgetKeys = ['ImageSplat', 'ImageSwipe', 'ImageSoundSwipe', 'PubMedNLP', 'TemplateWidget', 'ImageSwipeChoices'];
     const inList = widgetKeys.indexOf(config.widgetType) > -1;
     expect(inList).to.equal(true);
   });
