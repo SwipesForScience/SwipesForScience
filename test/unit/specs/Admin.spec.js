488103d5b4f81693257a6d0a05aa7b4483527200
akeshavan <keshavan@berkeley.edu>
2018-11-07 12:59:05 -0800

    fix: kind of works, but the mounted method doesn't complete

--- test/unit/specs/Admin.spec.js
+++ test/unit/specs/Admin.spec.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Admin from '@/components/Admin';
-import flushPromises from 'flush-promises';
+// import flushPromises from 'flush-promises';
 import firebase from 'firebase';
 
 // const FirebaseServer = require('firebase-server');
@@ -35,12 +35,11 @@ describe('Admin.vue', () => {
     // await vm.$nextTick();
 
     // so instead lets load sampleCounts ourselves and set the status.
-    vm.sampleCounts = { a: 0, b: 0, c: 0 };
+    vm.sampleCounts = [1, 2, 3, 4, 5];
     vm.status = 'complete';
-    // this doesn't work either...
     await vm.$nextTick();
 
     const paragraphs = vm.$el.getElementsByTagName('p');
-    expect(paragraphs[0].textContent).to.equal('Data Source: https://mydatasource.com');
+    expect(paragraphs[0].textContent).to.equal('You have 5 items currently');
   });
 });
