f51c6572f16fef711ed57751bb443c14defac058
cheryl <cheryl@francais@gmail.com>
2020-05-01 19:16:31 +0800

    updates tests for leaderboard and footer

--- test/unit/specs/Leaderboard.spec.js
+++ test/unit/specs/Leaderboard.spec.js
@@ -25,11 +25,4 @@ describe('Leaderboard.vue', () => {
     expect(vm.$el.querySelector('h1').textContent).to.equal('Leaderboard');
   });
 
-  it('should have correct image path', () => {
-    const vm = new Constructor({
-      propsData,
-    }).$mount();
-
-    expect(vm.$el.querySelector('img').src).to.equal(propsData.levels.sample_level.img);
-  });
 });
