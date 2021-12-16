6574f148b3727288de9384e48e0d1c53dd513933
Barry J. Tikalsky <tikal004@umn.edu>
2021-12-16 10:44:25 -0600

    env variables working in config/dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  AWS_ID: '"ZBWI7FYEARY7RW8L00E8"',
+  AWS_KEY: '"cdCbJgZDJT5UAVs3Cfc0HRAmLGyf1LlAdUXN0iKj"'
 })
