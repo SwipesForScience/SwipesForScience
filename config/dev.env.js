ac439fe456215d26b7d0b2b89881baa87d8bc1da
Barry J. Tikalsky <tikal004@umn.edu>
2021-12-16 12:14:53 -0600

    removed dev.env.js

--- config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  AWS_ID: '"TEST_ID"',
-  AWS_KEY: '"TEST_KEY"'
-})
