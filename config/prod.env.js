df0b138470cfb5530e5757ede358a093dd4f487e
Barry J. Tikalsky <tikal004@umn.edu>
2021-12-17 10:49:56 -0600

    removed stringify

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  VUE_APP_AWS_ID: JSON.stringify(process.env.VUE_APP_AWS_ID),
-  VUE_APP_AWS_KEY: JSON.stringify(process.env.VUE_APP_AWS_KEY)
+  VUE_APP_AWS_ID: process.env.VUE_APP_AWS_ID,
+  VUE_APP_AWS_KEY: process.env.VUE_APP_AWS_KEY
 }
