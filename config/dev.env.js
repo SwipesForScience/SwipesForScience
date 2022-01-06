582a0b3fc7f4e4a013cff479e422c0c4e59ea0f7
Barry J. Tikalsky <tikal004@umn.edu>
2022-01-06 16:23:03 -0600

    Removing MSI credentials from env files

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,4 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  VUE_APP_AWS_ID: process.env.VUE_APP_AWS_ID,
-  VUE_APP_AWS_KEY: process.env.VUE_APP_AWS_KEY
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,4 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  VUE_APP_AWS_ID: process.env.VUE_APP_AWS_ID,
-  VUE_APP_AWS_KEY: process.env.VUE_APP_AWS_KEY
 }
